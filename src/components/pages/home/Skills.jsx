47f6329e4e7557f6426a37dc9e58d2ab55667c41
rabby4 <mdrabbybd4747@gmail.com>
2023-12-11 22:42:12 +0600

    added skill section

--- src/components/pages/home/Skills.jsx
+++ src/components/pages/home/Skills.jsx
@@ -7,59 +7,82 @@ import react from '../../../assets/react.png'
 const Skills = () => {
     return (
         <>
-            <div className='container max-w-7xl mx-auto grid grid-cols-5 gap-10 my-36'>
+            <div className='container max-w-7xl mx-auto grid grid-cols-6 gap-10 my-36'>
                 <div className='col-span-2 text-left'>
                     <div className='space-y-5'>
-                        <h4 className='text-xl newFont'>My Skills</h4>
-                        <h2 className='text-4xl capitalize font-bold '>I am a front-end developer</h2>
+                        {/* <h4 className='text-xl newFont'>My Skills</h4> */}
+                        <h2 className='text-5xl capitalize font-bold '>My Skills</h2>
                         <div className="divider w-40 divider-warning h-0"></div>
-                        <p className='text-xl max-w-4xl mx-auto paragraph'>I am a front-end web developer and I am passionate about web design and development.</p>
+                        <p className='text-lg max-w-4xl mx-auto paragraph'>I am a front-end web developer and I am passionate about web design and development.</p>
                     </div>
                 </div>
-                <div className='col-span-3 grid grid-cols-3 gap-10'>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 90 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>90%</h3>
+                <div className='col-span-4 grid grid-cols-4 gap-5'>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 90 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>90%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>HTML5</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#F06529]'>HTML5</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 90 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>90%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 90 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>90%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>CSS3</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#264de4]'>CSS3</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 75 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>85%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 85 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>85%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>Tailwind</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#27b8bd]'>Tailwind</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 75 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>80%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 80 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>80%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>BootStrap</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#e040fb]'>BootStrap</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 70 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>70%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 70 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>70%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>JavaScript</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#f0db4f]'>JavaScript</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 70 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>70%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-28 h-28" style={{ "--value": 70 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>70%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>React JS</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#61dbfb]'>React JS</h3>
                     </div>
-                    <div className='flex flex-col justify-center items-center shadow-lg rounded-lg py-10'>
-                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 70 }} role="progressbar">
-                            <h3 className='text-3xl font-bold'>70%</h3>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 60 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>60%</h3>
                         </div>
-                        <h3 className='text-xl font-semibold mt-5 italic'>HTML5</h3>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#3d9b35]'>MongoDB</h3>
                     </div>
-
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 50 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>50%</h3>
+                        </div>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#6cc24a]'>NodeJS</h3>
+                    </div>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 85 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>85%</h3>
+                        </div>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#21759b]'>WordPress</h3>
+                    </div>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 80 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>80%</h3>
+                        </div>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#faad4d]'>Wix</h3>
+                    </div>
+                    <div className='flex flex-col justify-center items-center shadow-md rounded-lg py-8'>
+                        <div className="radial-progress flex items-center justify-center w-32 h-32" style={{ "--value": 50 }} role="progressbar">
+                            <h3 className='text-2xl font-bold'>50%</h3>
+                        </div>
+                        <h3 className='text-xl font-semibold mt-2 italic text-[#ff7262]'>Figma</h3>
+                    </div>
                 </div>
             </div>
         </>
